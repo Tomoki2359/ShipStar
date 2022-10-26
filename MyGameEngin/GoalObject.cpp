aa8644502d61ded5169f3994e666e5fc0159a653
智輝 千田 <210784@jc-21.jp>
2022-10-26 18:27:34 +0900

    ゴール等の追加

--- MyGameEngin/GoalObject.cpp
+++ MyGameEngin/GoalObject.cpp
@@ -13,8 +13,10 @@ GoalObject::~GoalObject()
 void GoalObject::Initialize()
 {
 	//モデルデータのロード
-	hModel_ = Model::Load("Assets\\temporary\\Parrot.fbx");
+	hModel_ = Model::Load("Assets\\goal.fbx");
 	assert(hModel_ >= 0);
+	transform_.scale_.y = 0.75;
+	transform_.scale_.x = 0.75;
 }
 
 void GoalObject::Draw()
